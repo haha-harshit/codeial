c58f882612ac21d98f4cf5b17b614e984d7f6202
Harshit <geraharshitkumar@gmail.com>
2020-11-30 19:48:20 +0530

    First commit: create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        //interpolition: use(`${}`)
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`server is running on port: ${port}`);
+})
